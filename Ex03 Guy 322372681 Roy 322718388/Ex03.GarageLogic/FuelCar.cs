43326259453bd3fde3f42bff0051d9fed557d38f
GuyFirst <guyfirst19@gmail.com>
2025-05-18 12:14:23 +0300

    nisui

--- Ex03 Guy 322372681 Roy 322718388/Ex03.GarageLogic/FuelCar.cs
+++ Ex03 Guy 322372681 Roy 322718388/Ex03.GarageLogic/FuelCar.cs
@@ -2,17 +2,7 @@
 {
     internal class FuelCar : Car
     {
-        protected enum eCarGasType
-        {
-            Soler,
-            Octan95,
-            Octan96,
-            Octan98
-        }
+       public FuelVeichle FuelVeichle { get; set; }
 
-        protected eCarGasType m_GasType;
-
-        private float m_MaxFuelLevel;
-        public float CurrentFuelLevel { get; set; }
     }
 }
