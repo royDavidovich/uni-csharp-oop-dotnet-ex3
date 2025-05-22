56c6ece0343879c312d8698dd30bca1fc011de18
GuyFirst <guyfirst19@gmail.com>
2025-05-23 02:29:02 +0300

    added a new class - input validator

---
+++ Ex03 Guy 322372681 Roy 322718388/Ex03.ConsoleUI/InputValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class InputValidator
+{
+    public static void ValidateNonEmptyString(string i_Input, string i_FieldName)
+    {
+        if (string.IsNullOrWhiteSpace(i_Input))
+        {
+            throw new FormatException($"{i_FieldName} cannot be empty or whitespace."); 
+        }
+    }
+
+    public static void ValidateNumber(string i_Input, string i_FieldName)
+    {
+        if (!float.TryParse(i_Input, out _))
+        {
+            throw new FormatException($"{i_FieldName} must be a valid number."); 
+        }
+    }
+
+    public static void ValidateEnum(string i_Input, Type i_EnumType, string i_FieldName)
+    {
+        if (!Enum.IsDefined(i_EnumType, i_Input))
+        {
+            throw new FormatException($"{i_FieldName} is invalid."); 
+        }
+    }
+} 
